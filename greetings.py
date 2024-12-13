3d850ae3ffd16e9843d6c57a319ea980c8d9011c
Bratan2009 <matxramoff@gmail.com>
2024-12-13 20:11:25 +0300

    Add files via upload
    Grade 1. Этап 1: Задание 1 (я загуглил про sep='\n')

---
+++ greetings.py
@@ -0,0 +1,10 @@
+username = "Bratan@2009"
+title = "Name"
+content = "My name is matvey"
+status = "True"
+created_date = "13-12-2024"
+issue_date = "23-12-2024"
+sep='\n'
+print(" Имя пользователя:", username,sep,"Заголовок заметки:", title ,sep,"Описание заметки:", content,sep,
+      "Дата создания заметки", created_date,sep,"Дата истечения заметки (дедлайн):", issue_date
+)
